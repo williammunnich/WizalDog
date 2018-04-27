35c7e19c671e4f30c981b85c8508db9e5b790a3b
williammunnich <williammunnich@gmail.com>
2018-04-27 16:58:43 -0500

    startin out

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(6 + 1);
+	}
+	
+}
---
+++ src/NoteTester.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class NoteTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			final JFrame frame = new JFrame("https://www.youtube.com/watch?v=lhcVdnQ_R1U");
+			JPanel pane = new JPanel();
+			fianl JLabel textLabel 
+	}
+
+}
